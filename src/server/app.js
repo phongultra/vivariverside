3890ec15d5d06af7fcba28b289eaa4dd8997f06c
Phong Nguyen <phongultra@gmail.com>
2018-03-24 17:20:54 +0700

    Data

--- src/server/app.js
+++ src/server/app.js
@@ -2,6 +2,6 @@ import app from './configs/server_config';
 import connection from './configs/const';
 var express = require('express');
 
-var server = app.listen(connection.portServer, () => {
-    console.log(`Server is running on port: ${connection.portServer}`);
+var server = app.listen(9100, () => {
+    console.log(`Server is running on port: 9100`);
 });
